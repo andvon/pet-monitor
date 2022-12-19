7bfbb69aa5d3188efeb7c4ee6509672a8f4fb9c1
avhgithub <vonhandorfa@gmail.com>
2022-12-18 19:43:32 -0500

    Fixed time issue

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import numpy as np
 from datetime import datetime
 from time import time
 from pathlib import Path,PurePath
+import pytz
 
 INSULIN_CSV = Path("data/insulin_data.csv")
 POO_CSV = Path("data/poo_data.csv")
@@ -11,7 +12,9 @@ PEE_CSV = Path("data/pee_data.csv")
 FOOD_CSV = Path("data/food_data.csv")
 WATER_CSV = Path("data/water_data.csv")
 
+EST = pytz.timezone('US/Eastern')
 overview_tab,restroom_tab, feeding_tab, insulin_tab = st.tabs(["Overview","Restroom","Feeding","Insulin"])
+
 
 
 if 'pee_df' not in st.session_state:
@@ -21,7 +24,7 @@ if 'pee_df' not in st.session_state:
 		st.session_state.pee_df = pd.read_csv(PEE_CSV)
 st.session_state.pee_df['date'] = pd.to_datetime(st.session_state.pee_df['date'])
 st.session_state.last_pee = st.session_state.pee_df.date[0] if len(st.session_state.pee_df.index) > 0 else None
-st.session_state.last_pee_diff = f'{round(((datetime.now() - st.session_state.pee_df.date[0]).total_seconds() / 3600),2)} Hours' if len(st.session_state.pee_df.index) > 0 else None
+st.session_state.last_pee_diff = f'{round(((datetime.now(EST) - st.session_state.pee_df.date[0]).total_seconds() / 3600),2)} Hours' if len(st.session_state.pee_df.index) > 0 else None
 
 if 'poo_df' not in st.session_state:
 	if not POO_CSV.is_file():
@@ -30,7 +33,7 @@ if 'poo_df' not in st.session_state:
 		st.session_state.poo_df = pd.read_csv(POO_CSV)
 st.session_state.poo_df['date'] = pd.to_datetime(st.session_state.poo_df['date'])
 st.session_state.last_poo = st.session_state.poo_df.date[0] if len(st.session_state.poo_df.index) > 0 else None
-st.session_state.last_poo_diff = f'{round(((datetime.now() - st.session_state.poo_df.date[0]).total_seconds() / 3600),2)} Hours' if len(st.session_state.poo_df.index) > 0 else None
+st.session_state.last_poo_diff = f'{round(((datetime.now(EST) - st.session_state.poo_df.date[0]).total_seconds() / 3600),2)} Hours' if len(st.session_state.poo_df.index) > 0 else None
 
 if 'food_df' not in st.session_state:
 	if not FOOD_CSV.is_file():
@@ -39,7 +42,7 @@ if 'food_df' not in st.session_state:
 		st.session_state.food_df = pd.read_csv(FOOD_CSV)
 st.session_state.food_df['date'] = pd.to_datetime(st.session_state.food_df['date'])
 st.session_state.last_feeding = st.session_state.food_df.date[0] if len(st.session_state.food_df.index) > 0 else None
-st.session_state.last_feeding_diff = f'{round(((datetime.now() - st.session_state.food_df.date[0]).total_seconds() / 3600),2)} Hours' if len(st.session_state.food_df.index) > 0 else None
+st.session_state.last_feeding_diff = f'{round(((datetime.now(EST) - st.session_state.food_df.date[0]).total_seconds() / 3600),2)} Hours' if len(st.session_state.food_df.index) > 0 else None
 
 if 'water_df' not in st.session_state:
 	if not WATER_CSV.is_file():
@@ -48,7 +51,7 @@ if 'water_df' not in st.session_state:
 		st.session_state.water_df = pd.read_csv(WATER_CSV)
 st.session_state.water_df['date'] = pd.to_datetime(st.session_state.water_df['date'])
 st.session_state.last_watering = st.session_state.water_df.date[0] if len(st.session_state.water_df.index) > 0 else None
-st.session_state.last_watering_diff = f'{round(((datetime.now() - st.session_state.water_df.date[0]).total_seconds() / 3600),2)} Hours' if len(st.session_state.water_df.index) > 0 else None
+st.session_state.last_watering_diff = f'{round(((datetime.now(EST) - st.session_state.water_df.date[0]).total_seconds() / 3600),2)} Hours' if len(st.session_state.water_df.index) > 0 else None
 
 if 'insulin_df' not in st.session_state:
 	if not INSULIN_CSV.is_file():
@@ -57,7 +60,7 @@ if 'insulin_df' not in st.session_state:
 		st.session_state.insulin_df = pd.read_csv(INSULIN_CSV)
 st.session_state.insulin_df['date'] = pd.to_datetime(st.session_state.insulin_df['date'])
 st.session_state.last_insulin = st.session_state.insulin_df.date[0] if len(st.session_state.insulin_df.index) > 0 else None
-st.session_state.last_insulin_diff = f'{round(((datetime.now() - st.session_state.insulin_df.date[0]).total_seconds() / 3600),2)} Hours' if len(st.session_state.insulin_df.index) > 0 else None
+st.session_state.last_insulin_diff = f'{round(((datetime.now(EST) - st.session_state.insulin_df.date[0]).total_seconds() / 3600),2)} Hours' if len(st.session_state.insulin_df.index) > 0 else None
 
 
 with overview_tab:
@@ -109,11 +112,11 @@ with restroom_tab:
 			st.header("Pee")
 			pee_button = st.button("Record Pee")
 			if pee_button:
-				pee_date_time = datetime.now()
+				pee_date_time = datetime.now(EST)
 				st.session_state.pee_df = st.session_state.pee_df.append({'date': pee_date_time}, ignore_index=True).sort_values(['date'],ascending = False,ignore_index=True)
 				st.session_state.pee_df.to_csv(PEE_CSV,index = False)
 				st.session_state.last_pee = st.session_state.pee_df.date[0]
-				st.session_state.last_pee_diff = f'{round(((datetime.now() - st.session_state.last_pee).total_seconds() / 3600),2)} Hours'
+				st.session_state.last_pee_diff = f'{round(((pee_date_time - st.session_state.last_pee).total_seconds() / 3600),2)} Hours'
 
 		with col2:
 			st.header("Poo")
@@ -122,14 +125,14 @@ with restroom_tab:
 			poo_type = st.selectbox("Type",("normal","runny","other"))
 			if poo_button:
 				poo_items = {
-					"date": datetime.now(),
+					"date": datetime.now(EST),
 					"size": poo_size,
 					"type": poo_type
 				}
 				st.session_state.poo_df = st.session_state.poo_df.append(poo_items, ignore_index=True).sort_values(['date'],ascending = False,ignore_index=True)
 				st.session_state.poo_df.to_csv(POO_CSV,index = False)
 				st.session_state.last_poo = st.session_state.poo_df.date[0]
-				st.session_state.last_poo_diff = f'{round(((datetime.now() - st.session_state.last_poo).total_seconds() / 3600),2)} Hours'
+				st.session_state.last_poo_diff = f'{round(((poo_items["date"] - st.session_state.last_poo).total_seconds() / 3600),2)} Hours'
 
 	with st.container():
 		col1,col2 = st.columns(2)
@@ -150,13 +153,13 @@ with feeding_tab:
 			food_cups = st.number_input("Food amount (cups)", min_value=0.0, max_value=5.0, value=0.5, step=0.1)
 			if food_button:
 				food_items = {
-					"date": datetime.now(),
+					"date": datetime.now(EST),
 					"cups": food_cups
 				}
 				st.session_state.food_df = st.session_state.food_df.append(food_items, ignore_index=True).sort_values(['date'],ascending = False,ignore_index=True)
 				st.session_state.food_df.to_csv(FOOD_CSV,index = False)
 				st.session_state.last_feeding = st.session_state.food_df.date[0]
-				st.session_state.last_feeding_diff = f'{round(((datetime.now() - st.session_state.last_feeding).total_seconds() / 3600),2)} Hours'
+				st.session_state.last_feeding_diff = f'{round(((food_items["date"] - st.session_state.last_feeding).total_seconds() / 3600),2)} Hours'
 
 		with col2:
 			st.subheader("Water")
@@ -170,7 +173,7 @@ with feeding_tab:
 				water_added_vol = 1000
 			if water_button:
 				water_items = {
-					"date": datetime.now(),
+					"date": datetime.now(EST),
 					"remaining ml": water_remaining_vol,
 					"added ml": water_added_vol,
 					"record type": water_change_type
@@ -178,7 +181,7 @@ with feeding_tab:
 				st.session_state.water_df = st.session_state.water_df.append(water_items, ignore_index=True).sort_values(['date'],ascending = False,ignore_index=True)
 				st.session_state.water_df.to_csv(WATER_CSV,index = False)
 				st.session_state.last_watering = st.session_state.water_df.date[0]
-				st.session_state.last_watering_diff = f'{round(((datetime.now() - st.session_state.last_watering).total_seconds() / 3600),2)} Hours'
+				st.session_state.last_watering_diff = f'{round(((water_items["date"] - st.session_state.last_watering).total_seconds() / 3600),2)} Hours'
 	
 	with st.container():
 		col1,col2 = st.columns(2)
@@ -201,7 +204,7 @@ with insulin_tab:
 	if insulin_record:
 		# update dataframe state
 		insulin_items = {
-					"date": datetime.now(),
+					"date": datetime.now(EST),
 					"dose administered (units)": insulin_dose,
 					"brand": insulin_brand,
 					"user": insulin_user
@@ -209,7 +212,7 @@ with insulin_tab:
 		st.session_state.insulin_df = st.session_state.insulin_df.append(insulin_items, ignore_index=True).sort_values(['date'],ascending = False,ignore_index=True)
 		st.session_state.insulin_df.to_csv(INSULIN_CSV,index = False)
 		st.session_state.last_insulin = st.session_state.insulin_df.date[0]
-		st.session_state.last_insulin_diff = f'{round(((datetime.now() - st.session_state.last_insulin).total_seconds() / 3600),2)} Hours'
+		st.session_state.last_insulin_diff = f'{round(((insulin_items["date"] - st.session_state.last_insulin).total_seconds() / 3600),2)} Hours'
 	st.write("--------------")
 	st.subheader("Recent Insulin Injection Records")
 	st.write(st.session_state.insulin_df.head(10))
